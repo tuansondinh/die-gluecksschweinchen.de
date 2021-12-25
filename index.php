d4cf6c8beab7be7cc5cc64e7cf2f5eb76bbda22b
Tuan Son Dinh <tuansondinh96@gmail.com>
2021-12-25 11:53:46 +0100

    use js scripts from internet

--- index.php
+++ index.php
@@ -6,24 +6,22 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" type="text/css" href="css/index.css"/>
-    <script src="https://kit.fontawesome.com/c74022c18c.js" crossorigin="anonymous"></script>  
+    <script src="https://kit.fontawesome.com/c74022c18c.js" crossorigin="anonymous"></script>
     <meta property="og:title" content="Urlaubsbetreuung für Meerschweinchen">
   </head>
 
-  <body class=""> 
+  <body class="">
     <div id="" class="container">
       <?php require("./parts/header.php"); ?>
-      <?php require("./parts/navbar.php"); ?> 
+      <?php require("./parts/navbar.php"); ?>
       <?php require("./parts/home.php"); ?>
       <?php require("./parts/slider.php"); ?>
       <?php require("./parts/footer.php"); ?>
     </div>
-  
-    <script
-      src="https://code.jquery.com/jquery-2.2.4.min.js"
-      integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
-      crossorigin="anonymous"></script>
-    <script type="text/javascript" src="bootstrap-4.4.1/dist/js/bootstrap.bundle.min.js"></script>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <!-- <script type="text/javascript" src="javascript/fade.js" ></script> -->
   </body>
-</html>    
+</html>
