d3db294e9d9974d0709c3ff016f24dc61a8913ab
Tuan Son Dinh <tuansondinh96@gmail.com>
2022-06-02 21:29:45 +0200

    change price.php

--- parts/prices.php
+++ parts/prices.php
@@ -6,10 +6,13 @@
     <p>Wir berechnen <span class="font-weight-bold">5,00 EUR pro Nacht und Tier</span> für die Betreuung.
     </p> 
 
-    <p>Sonderabsprachen ab dem dritten Tier bei längeren Aufenthalten möglich.
+    <p>Sonderabsprachen bei größeren Gruppen und längeren Aufenthalten möglich.
     </p>
     <p>
 Diese Kosten sind ein All-Inklusive-Preis, der das Frischfutter und das Heu, die Einstreu, die Endreinigung des Käfigs/Geheges und aller Einrichtungsgegenstände (gemäß Desinfektionsplan) sowie die Abfallentsorgung beinhaltet.
+</p>
+<p>
+Der Mindestaufenthalt beträgt 5 Nächte, da jedes Urlaubsquartier anschließend aufwändig gereinigt und desinfiziert werden muss. Wenn die Aufenthaltsdauer kürzer ist, sind trotzdem die Kosten für 5 Nächte zu entrichten. 
 </p>
 
     <!-- <button type="button" class="btn btn-primary">Erfahre mehr!</button> -->
