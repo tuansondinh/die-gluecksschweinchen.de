8f476d4111557a9d0e8ae775a55940601cb1e603
Tuan Son Dinh <tuansondinh96@gmail.com>
2022-11-19 22:01:52 +0100

    change prices

--- parts/prices.php
+++ parts/prices.php
@@ -2,18 +2,16 @@
   
   <div class="inhalt bg-light col-md-8 order-md-2 justify-content-center">
 
-    <p class="ueberschrift">Preise(ab 01.01.2022):</p>
-    <p>Wir berechnen <span class="font-weight-bold">5,00 EUR pro Nacht und Tier</span> für die Betreuung.
-    </p> 
 
-    <p>Sonderabsprachen bei größeren Gruppen und längeren Aufenthalten möglich.
-    </p>
-    <p>
-Diese Kosten sind ein All-Inklusive-Preis, der das Frischfutter und das Heu, die Einstreu, die Endreinigung des Käfigs/Geheges und aller Einrichtungsgegenstände (gemäß Desinfektionsplan) sowie die Abfallentsorgung beinhaltet.
-</p>
-<p>
-Der Mindestaufenthalt beträgt 5 Nächte, da jedes Urlaubsquartier anschließend aufwändig gereinigt und desinfiziert werden muss. Wenn die Aufenthaltsdauer kürzer ist, sind trotzdem die Kosten für 5 Nächte zu entrichten. 
-</p>
+<p class="ueberschrift">Preise (ab 01.01.2023):</p>
+
+<p>Aufgrund der enormen derzeitigen Entwicklungen im Hinblick auf die Gemüse-, Heu- und Einstreupreise sowie der erheblich gestiegenen Unterhaltungs- und Energiekosten berechnen wir zukünftig <span class="font-weight-bold">5,50 EUR pro Nacht und Tier</span> für die Betreuung.</p>
+
+<p>Sonderabsprachen bei größeren Gruppen und längeren Aufenthalten möglich.</p>
+
+<p>Die Betreuungskosten sind ein <span class="font-weight-bold">All-Inklusive-Preis</span>, der das Frischfutter und das Heu, die Einstreu, die Zwischen- und Endreinigung des Käfigs/Geheges und aller Einrichtungsgegenstände (gemäß Desinfektionsplan) sowie die Mistentsorgung beinhaltet.</p>
+
+<p>Der <span class="font-weight-bold">Mindestaufenthalt beträgt bei zwei Tieren 7 Nächte</span>, da jedes Urlaubsquartier anschließend aufwändig gereinigt und desinfiziert werden muss. Wenn die Aufenthaltsdauer kürzer ist, sind trotzdem die Kosten für 7 Nächte zu entrichten. Der Mindestaufenthalt beträgt bei <span class="font-weight-bold">drei und mehr Tieren 5 Nächte</span>. Wenn die Aufenthaltsdauer kürzer ist, sind trotzdem die Kosten für 5 Nächte zu entrichten.</p>
 
     <!-- <button type="button" class="btn btn-primary">Erfahre mehr!</button> -->
  
